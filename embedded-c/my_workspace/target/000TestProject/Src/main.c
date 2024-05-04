126d62c9ebae5fa40763734899f42d47cf2e95d2
Nich0l3 <146418190+Nich0l3@users.noreply.github.com>
2024-05-04 20:39:43 +0530

    led on : target

--- embedded-c/my_workspace/target/000TestProject/Src/main.c
+++ embedded-c/my_workspace/target/000TestProject/Src/main.c
@@ -16,15 +16,17 @@
  ******************************************************************************
  */
 
-#include <stdint.h>
+#include <stdio.h>
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+extern void initialise_monitor_handles(void);
+
 int main(void)
 {
-    /* Loop forever */
+	initialise_monitor_handles();
+	printf("Hello world!!");
 	for(;;);
-
 }
