ab1e961fb6634c9c210b66754ca1956d377fa1bf
Nich0l3 <146418190+Nich0l3@users.noreply.github.com>
2024-05-27 18:02:23 +0530

    update

---
+++ embedded-c/basics/030-while-even.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdint.h>
+
+void smooth_exit(void);
+
+int main(int argc, char **argv){
+
+	int32_t input1, input2;
+	
+	uint32_t count = 0;
+
+	printf("Enter two numbers : ");
+	scanf("%d %d", &input1, &input2);
+
+	if(input1 > input2){
+		
+		printf("Ending number > starting number\n");
+		smooth_exit();
+		return -1;
+	}
+	
+	printf("Even numbers are :\n");
+
+	while(input1 <= input2){
+		if(!(input1%2)){
+			printf("%4d\t",input1);
+			count++;
+		}	
+		input1++;
+	}
+
+	printf("\nTotal even numbers : %u\n", count);
+	smooth_exit();
+}
+
+
+void smooth_exit(void){
+	printf("Enter return to exit the application");
+	while(getchar() != '\n');
+	getchar();
+}
